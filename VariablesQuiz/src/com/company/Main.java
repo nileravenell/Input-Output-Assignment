7e57efe36b836477c318c79cdc2b231b16af7b13
Nile Ravenell <43184722+nileravenell@users.noreply.github.com>
2020-12-03 19:48:25 -0500

    Add files via upload

---
+++ VariablesQuiz/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int quantity = 1;
+            String fruit = "Apple";
+            float price =2.99f;
+            boolean rotten = false;
+
+            System.out.println("Quantity is "+ quantity + ".");
+            System.out.println("It is a " + fruit + ".");
+            System.out.println("The price is " + price + ".");
+            System.out.println("Is the fruit rotten? " + rotten);
+    }
+}
